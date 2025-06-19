d10cfb7c96b6a6f0d6866c5cfbd36ac0d08d98a6
raviimk <deathmaniya@gmail.com>
2025-06-19 16:03:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -161,6 +161,10 @@ const App = () => {
   };
   const gujaratiShape = shapeMap[last.shape?.toUpperCase()] || last.shape;
 
+  const totalCent = diamonds.reduce((sum, d) => sum + d.centWeight, 0).toFixed(3);
+  const totalCarat = diamonds.reduce((sum, d) => sum + d.caratWeight, 0).toFixed(3);
+  const totalPcs = diamonds.length;
+
   const html = `
     <html>
       <head>
